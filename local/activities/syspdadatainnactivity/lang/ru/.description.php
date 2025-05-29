3cc02524f05c92de3a041a53a6a16b1a05f9d666
5152 <5152@mail.ru>
2025-05-29 23:24:57 +0700

    feat(dadata_activity): implements dadata inn search bizproc activity

---
+++ local/activities/syspdadatainnactivity/lang/ru/.description.php
@@ -0,0 +1,4 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+$MESS['SYSPDADATAINN_DESCR_NAME']  = 'Поиск по ИНН (dadata)';
+$MESS['SYSPDADATAINN_DESCR_DESCR'] = 'Получение названия компании по ИНН через сервис dadata.ru';
+$MESS['SYSPDADATAINN_FIELD_COMPANY'] = 'Компания';
