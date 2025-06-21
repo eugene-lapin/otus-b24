1df9b27e296d0bf5e60fdf604294b6d6f441393c
5152 <5152@mail.ru>
2025-06-21 17:36:38 +0700

    chore(global): Delete bitrix files

--- doctors/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
-
-\Bitrix\Main\Page\Asset::getInstance()->addCss('/local/assets/css/doctors.css');
-require_once 'router.php';
-
-require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
--- otus/debug.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
-
-$date = date('Y-m-d G:i:s');
-file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/logs/debug.log', $date . PHP_EOL, FILE_APPEND);
-
-require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
